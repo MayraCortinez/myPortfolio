5340da40f334e7d465ca03ceaea940bd74fd077d
MayraCortinez <mayrautopia@gmail.com>
2024-07-24 21:52:50 -0300

    delete files

--- src/scripts/nav.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('projects-button').addEventListener('click', () => {
-    const menu = document.getElementById('projects-menu');
-    menu.classList.toggle('hidden');
-  });
--- src/utils/setupMenuToggle.js
+++
@@ -1,9 +0,0 @@
-export function setupMenuToggle() {
-    const menuButton = document.getElementById('menu-button');
-    const mobileMenu = document.getElementById('mobile-menu');
-  
-    menuButton.addEventListener('click', function() {
-      mobileMenu.classList.toggle('hidden');
-    });
-  }
-  
